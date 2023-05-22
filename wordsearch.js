334132012ad56fe3b6bad58a4f9ef95de39cfcf6
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-21 23:30:10 -0700

    updated wordsearch to look for words vertically as well

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// pair programming done with @mhosseinsafi
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let vertStr = "";
+    for (let j = 0; j < letters.length; j++) {
+      vertStr += letters[j][i];
+    }
+    verticalJoin.push(vertStr);
+  }
+  for (const l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
